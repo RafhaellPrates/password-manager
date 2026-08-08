80449f94bf7d8ce505afa43c4738b6fb42d3fe89
Rafhael Prates <fellepy960@gmail.com>
2026-08-07 23:08:08 -0300

    feat(backend): adiciona cadastro de usuario POST /auth/register
    Closes#4

---
+++ backend/controllers/healthController.js
@@ -0,0 +1,14 @@
+import pool from "../db/index.js";
+
+const health = async (req,res)  => {
+
+    try{
+        await pool.query('SELECT 1');
+        
+        res.sendStatus(200)
+        
+    } catch (err){
+        res.status(500).json({erro:'Erro interno'})
+    }
+}
+export default health
